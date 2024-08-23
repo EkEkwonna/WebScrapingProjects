24765582c9e5d95fe90676666cc58e3afae85bec
EkEkwonna <kennyekwonna@gmail.com>
2024-08-23 20:37:28 +0100

    kinda ready

--- Selenium/myer-david-jones.py
+++ Selenium/myer-david-jones.py
@@ -20,10 +20,16 @@ data = []
 "Function used for checking specific product attributes"
 
 def check_product(element_type,field,attribute_detail):
-    WebDriverWait(browser,20).until(EC.presence_of_element_located((By.XPATH,f"//{element_type}[@{field} = '{attribute_detail}']")))
+    WebDriverWait(browser,100000).until(EC.presence_of_element_located((By.XPATH,f"//{element_type}[@{field} = '{attribute_detail}']")))
     if browser.find_elements(By.XPATH,f"//{element_type}[@{field} = '{attribute_detail}']") != []:
-        return browser.find_element(By.XPATH,f"//{element_type}[@{field} = '{attribute_detail}']").text
+        WebDriverWait(browser,100000).until(EC.presence_of_element_located((By.XPATH,f"//{element_type}[@{field} = '{attribute_detail}']")))
+        output = browser.find_element(By.XPATH,f"//{element_type}[@{field} = '{attribute_detail}']").text 
+        return output
     else:
+        # print('=========NOT WORKING=========')
+        # print(browser.current_url)
+        # print(f"//{element_type}[@{field} = '{attribute_detail}']")
+        # print('=========NOT WORKING=========')
         return ''
 
 def extract_description():
@@ -38,7 +44,7 @@ def extract_description():
 "[Item , Description (including unit of measure), Current price, previous price, Size , Colour , discount]"
 def extract_myer_details():
     title = check_product('h1','class','css-11xqsrc')
-    current_display_price = check_product('p','class','css-1mxfaop')
+    current_display_price = check_product('p','data-automation','product-price-now')
     discount = check_product('p','class','discount-text')
     previous_price = check_product('p','data-automation','product-price-was')
     colour = check_product('span','data-automation','pdp-colour-display-value')
@@ -54,10 +60,12 @@ def extract_david_jones_details():
     current_display_price = check_product('span','class','price-display')
     description = check_product('div','class','content long-description ql-editor')
     colour = check_product('span','class','colour-label')
-    size = check_product('div','class','size-selection')
+    size = check_product('div','class','size-selection')[4:]
     previous_price = ''
     discount = ''
     row = [title,description,current_display_price,previous_price,colour,size,discount]
+    print(row)
+    return row
 
 
 
@@ -65,7 +73,7 @@ def extract_david_jones_details():
 "==========================================================="
 "Initiating Webscraping"
 
-browser.get('https://www.myer.com.au')
+# browser.get('https://www.myer.com.au')
 
 "Random Sample list for www.myer.com.au"
 
@@ -76,25 +84,24 @@ myer_list = ["Organic Cotton Wardrobe Staple Long Sleeve Tee In Khaki",
              "Lightsaber Squad Extendable Toy Lightsaber Assorted",
              "Star Wars Darth Vader Mech 75368",
              "SoundLink Revolve II Bluetooth Speaker 858365-0100",
-             "Citiz & Milk Capsule Coffee Machine EN267BAE",
-             "Bloom Quilt Cover Set in Stone"]
+             "Citiz & Milk Capsule Coffee Machine EN267BAE"]
 
-# for product in myer_list:
-#     page_number = 1
-#     browser.get(f'https://www.myer.com.au/search?query={product.replace(" ","+").replace("&","%26")}&pageNumber={page_number}')
-#     item_found = 'No'
+for product in myer_list:
+    page_number = 1
+    browser.get(f'https://www.myer.com.au/search?query={product.replace(" ","+").replace("&","%26")}&pageNumber={page_number}')
+    item_found = 'No'
 
-#     while item_found == 'No':
-#         "Checking item in search results"
-#         WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.XPATH,"//div[@data-automation = 'products-container']")))
-#         container = browser.find_element(By.XPATH,"//div[@data-automation = 'products-container']")
+    while item_found == 'No':
+        "Checking item in search results"
+        WebDriverWait(browser, 1000000).until(EC.presence_of_element_located((By.XPATH,"//div[@data-automation = 'products-container']")))
+        container = browser.find_element(By.XPATH,"//div[@data-automation = 'products-container']")
         
-#         if container.find_elements(By.XPATH,f'.//p[text()=\"{product}\"]') !=[]:
-#             item_found = 'Yes'
-#             located_item = container.find_element(By.XPATH,f'.//p[text()=\"{product}\"]').click()
-#             data.append(extract_myer_details())
-#         else:
-#             page_number +=1
+        if container.find_elements(By.XPATH,f'.//p[text()=\"{product}\"]') !=[]:
+            item_found = 'Yes'
+            located_item = container.find_element(By.XPATH,f'.//p[text()=\"{product}\"]').click()
+            data.append(extract_myer_details())
+        else:
+            page_number +=1
 
 "Starting David Jones Extraction"
 
@@ -107,7 +114,6 @@ david_jones_list = ["T-RACE MOTOGP CHRONOGRAPH 2024 LIMITED EDITION WATCH",
                     "NEOCROC R LACOSTE BACKPACK SGNATURE",
                     "EXPLORAFUNK S MAILLE/CLF GLIT/SUE ME/CL ST/SP ASTR",
                     "MONO MUG",
-                    "STARWARD SOLERA SINGLE MALT WHISKY 700ML",
                     "SQUARE SIGNATURE GRILLIT 26CM SATIN BLACK",
                     "MENS ACONCAGUA 3 VEST",
                     "ITALIAN MOLESKIN ITEM JACKET"]
@@ -120,10 +126,9 @@ for product in david_jones_list:
         "Checking item in search results"
         WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.XPATH,"//div[@class = 'products']")))
         container = browser.find_element(By.XPATH,"//div[@class = 'products']")
-        print('here')
-        if container.find_elements(By.XPATH,f'//a[text() = \"{product.title()}\"]') != []:
+        if container.find_elements(By.XPATH,f'//a[text() = \"{product}\"]') != []:
             item_found = 'Yes'
-            located_item = browser.find_element(By.XPATH,f'//a[text() = \"{product.title()}\"]').click()
+            located_item = browser.find_element(By.XPATH,f'//a[text() = \"{product}\"]').click()
             data.append(extract_david_jones_details())
         elif container.find_elements(By.XPATH,f'//a[text() = \"{string.capwords(product)}\"]') != []:
             item_found = 'Yes'
@@ -132,8 +137,8 @@ for product in david_jones_list:
 
 
 
-# "Producing output CSV File"
+"Producing output CSV File"
 
-# print(len(data),' rows collected')
-# df = pd.DataFrame(data,columns=['Title','Description','Current Price','Previous Price','Colour','Discount'])
-# df.to_csv('myer-david-jones.csv',index=False)
+print(len(data),' rows collected')
+df = pd.DataFrame(data,columns=['Title','Description','Current Price','Previous Price','Colour','Size','Discount'])
+df.to_csv('myer-david-jones.csv',index=False)
