633537a540b7a92e5184aea859b9518086d78d74
EkEkwonna <kennyekwonna@gmail.com>
2024-08-23 19:37:53 +0100

    updated script

--- Selenium/myer-david-jones.py
+++ Selenium/myer-david-jones.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 
+import string
 import pandas as pd
 options = Options()
 options.headless = False
@@ -42,9 +43,22 @@ def extract_myer_details():
     previous_price = check_product('p','data-automation','product-price-was')
     colour = check_product('span','data-automation','pdp-colour-display-value')
     description = extract_description()
-    row = [title,description,current_display_price,previous_price,colour,discount]
+    size = ''
+    row = [title,description,current_display_price,previous_price,colour,size,discount]
     print(row)
     return row
+
+
+def extract_david_jones_details():
+    title = check_product('h1','itemprop','name')
+    current_display_price = check_product('span','class','price-display')
+    description = check_product('div','class','content long-description ql-editor')
+    colour = check_product('span','class','colour-label')
+    size = check_product('div','class','size-selection')
+    previous_price = ''
+    discount = ''
+    row = [title,description,current_display_price,previous_price,colour,size,discount]
+
 
 
 
@@ -65,53 +79,61 @@ myer_list = ["Organic Cotton Wardrobe Staple Long Sleeve Tee In Khaki",
              "Citiz & Milk Capsule Coffee Machine EN267BAE",
              "Bloom Quilt Cover Set in Stone"]
 
-for product in myer_list:
-    page_number = 1
-    browser.get(f'https://www.myer.com.au/search?query={product.replace(" ","+").replace("&","%26")}&pageNumber={page_number}')
+# for product in myer_list:
+#     page_number = 1
+#     browser.get(f'https://www.myer.com.au/search?query={product.replace(" ","+").replace("&","%26")}&pageNumber={page_number}')
+#     item_found = 'No'
+
+#     while item_found == 'No':
+#         "Checking item in search results"
+#         WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.XPATH,"//div[@data-automation = 'products-container']")))
+#         container = browser.find_element(By.XPATH,"//div[@data-automation = 'products-container']")
+        
+#         if container.find_elements(By.XPATH,f'.//p[text()=\"{product}\"]') !=[]:
+#             item_found = 'Yes'
+#             located_item = container.find_element(By.XPATH,f'.//p[text()=\"{product}\"]').click()
+#             data.append(extract_myer_details())
+#         else:
+#             page_number +=1
+
+"Starting David Jones Extraction"
+
+# browser.get('https://www.davidjones.com')
+
+"Random sample list for from www.davidjones.com"
+david_jones_list = ["T-RACE MOTOGP CHRONOGRAPH 2024 LIMITED EDITION WATCH",
+                    "SEASTAR 1000 POWERMATIC 80 40MM WATCH",
+                    "WOMEN'S SLOANE SANDAL",
+                    "NEOCROC R LACOSTE BACKPACK SGNATURE",
+                    "EXPLORAFUNK S MAILLE/CLF GLIT/SUE ME/CL ST/SP ASTR",
+                    "MONO MUG",
+                    "STARWARD SOLERA SINGLE MALT WHISKY 700ML",
+                    "SQUARE SIGNATURE GRILLIT 26CM SATIN BLACK",
+                    "MENS ACONCAGUA 3 VEST",
+                    "ITALIAN MOLESKIN ITEM JACKET"]
+
+for product in david_jones_list:
+    browser.get(f'https://www.davidjones.com/search?q={product}'.replace(' ','+').replace("&","%26"))
     item_found = 'No'
 
     while item_found == 'No':
         "Checking item in search results"
-        WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.XPATH,"//div[@data-automation = 'products-container']")))
-        container = browser.find_element(By.XPATH,"//div[@data-automation = 'products-container']")
-    
-
-        
-        if container.find_elements(By.XPATH,f'.//p[text()=\"{product}\"]') !=[]:
+        WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.XPATH,"//div[@class = 'products']")))
+        container = browser.find_element(By.XPATH,"//div[@class = 'products']")
+        print('here')
+        if container.find_elements(By.XPATH,f'//a[text() = \"{product.title()}\"]') != []:
             item_found = 'Yes'
-            located_item = container.find_element(By.XPATH,f'.//p[text()=\"{product}\"]').click()
-            data.append(extract_myer_details())
-        else:
-            page_number +=1
+            located_item = browser.find_element(By.XPATH,f'//a[text() = \"{product.title()}\"]').click()
+            data.append(extract_david_jones_details())
+        elif container.find_elements(By.XPATH,f'//a[text() = \"{string.capwords(product)}\"]') != []:
+            item_found = 'Yes'
+            located_item = browser.find_element(By.XPATH,f'//a[text() = \"{string.capwords(product)}\"]').click()
+            data.append(extract_david_jones_details())
 
 
 
-# browser.get('https://www.davidjones.com')
+# "Producing output CSV File"
 
-# "Random sample list for from www.davidjones.com"
-# david_jones_list = ["T-RACE MOTOGP CHRONOGRAPH 2024 LIMITED EDITION WATCH",
-#                     "SEASTAR 1000 POWERMATIC 80 40MM WATCH",
-#                     "WOMEN'S SLOANE SANDAL",
-#                     "NEOCROC R LACOSTE BACKPACK SGNATURE",
-#                     "EXPLORAFUNK S MAILLE/CLF GLIT/SUE ME/CL ST/SP ASTR",
-#                     "MONO MUG",
-#                     "STARWARD SOLERA SINGLE MALT WHISKY 700ML",
-#                     "SQUARE SIGNATURE GRILLIT 26CM SATIN BLACK",
-#                     "MENS ACONCAGUA 3 VEST",
-#                     "ITALIAN MOLESKIN ITEM JACKET"]
-
-# for product in david_jones_list:
-#     # WebDriverWait(browser, 100).until(EC.element_to_be_clickable((By.XPATH,"//input[contains(placeholder,'Search for product or brand')]")))
-#     search_tool = browser.find_element(By.XPATH,"//input[contains(placeholder,'Search for product or brand')]")
-#     search_tool.send_keys(product)
-#     search_tool.send_keys(Keys.ENTER)
-#     print(product)
-
-
-
-
-"Producing output CSV File"
-
-print(len(data),' rows collected')
-df = pd.DataFrame(data,columns=[title,description,current_display_price,previous_price,colour,discount])
-df.to_csv('myer-david-jones.csv',index=False)
+# print(len(data),' rows collected')
+# df = pd.DataFrame(data,columns=['Title','Description','Current Price','Previous Price','Colour','Discount'])
+# df.to_csv('myer-david-jones.csv',index=False)
