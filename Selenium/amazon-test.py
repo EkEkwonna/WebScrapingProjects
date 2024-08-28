523b18cd38156359e6bf125b1f680494288f8365
EkEkwonna <kennyekwonna@gmail.com>
2024-08-28 13:41:56 +0100

    Final Test

--- Selenium/amazon-test.py
+++ Selenium/amazon-test.py
@@ -109,9 +109,9 @@ def extract_all_details(attribute_dimensions):
     row = [ASIN_VALUE,Title,Description,Display_price,processing_time,Shipping_charge,Stock,Color,Size,Style]
     "If we have 2 adjecent rows with the same product title and same color no pictures selected"
 
-    print("------------------------------------------")
+
     print("-------------ADDING ROW-------------------")
-    print(ASIN_VALUE,':',Title,',',Color,',',Size,',',Style)
+    print(ASIN_VALUE,':',Title,',',Color,',',Size,',',Style,',',Display_price)
     print('------------------------------------------')
 
     
@@ -138,53 +138,55 @@ for item in ASIN_LIST:
     print('=====================================')
     print('Observing product with ASIN:',str(item))
     available_options_sections = browser.find_elements(By.XPATH,"//div[@id = 'centerCol']//ul[contains(@class,'a-unordered-list')]")
-    print(len(available_options_sections)-1,'Number of sections detected')
+
     if len(available_options_sections) == 2:
         section = browser.find_element(By.XPATH,"//div[@id = 'centerCol']//ul")
         section_options = [(item.get_attribute('id'),item.get_attribute('class'),item.get_attribute('title').split('Click to select ')[1]) for item in section.find_elements(By.XPATH, ".//li")]
-        print(section_options)
+        # print(section_options)
         for section_option in section_options:
             if section_option[1] != 'swatchUnavailable':
                 browser.find_element(By.XPATH,f"//li[@id ='{section_option[0]}']").click()
                 attribute_dimensions = [section_option[0].split('_')[0]]
                 print('Attributes detected:',section_option[0].split('_')[0])    
                 scrape_elements(attribute_dimensions)
+                browser.refresh()
 
     if len(available_options_sections) == 3:
         first_section = browser.find_element(By.XPATH,"//div[@id = 'centerCol']//ul[contains(@class,'a-unordered-list')]")
         first_section_options = [(list_item.get_attribute('id'),list_item.get_attribute('class'),list_item.get_attribute('title').split('Click to select ')[1]) for list_item in first_section.find_elements(By.XPATH,".//li")]
-        print("First Section",first_section_options)
+        # print("First Section",first_section_options)
         for first_section_item in first_section_options:
-            WebDriverWait(browser,100).until(EC.presence_of_element_located((By.XPATH,f"//li[@id ='{first_section_item[0]}']")))
+            WebDriverWait(browser,200).until(EC.presence_of_element_located((By.XPATH,f"//li[@id ='{first_section_item[0]}']")))
             browser.find_element(By.XPATH,f"//li[@id ='{first_section_item[0]}']").click()
             second_section = browser.find_elements(By.XPATH,"//div[@id = 'centerCol']//ul[contains(@class,'a-unordered-list')]")[1]
             second_section_options = [(list_item.get_attribute('id'),list_item.get_attribute('class'),list_item.get_attribute('title').split('Click to select ')[1]) for list_item in second_section.find_elements(By.XPATH,".//li")]
-            print('Second Section',second_section_options)
+            # print('Second Section',second_section_options)
             for second_section_option in second_section_options:
                 if second_section_option[1] != 'swatchUnavailable':
-                    WebDriverWait(browser,100).until(EC.element_to_be_clickable((By.XPATH,f"//li[@id = '{second_section_option[0]}']")))
+                    WebDriverWait(browser,200).until(EC.element_to_be_clickable((By.XPATH,f"//li[@id = '{second_section_option[0]}']")))
                     browser.find_element(By.XPATH,f"//li[@id = '{second_section_option[0]}']").click()
                     attribute_dimensions = [first_section_item[0].split('_')[0],second_section_option[0].split('_')[0]]
                     print('Attributes detected',first_section_item[0].split('_')[0],',',second_section_option[0].split('_')[0])
                     scrape_elements(attribute_dimensions)
+                    browser.refresh()
 
     if len(available_options_sections) ==4:
         first_section = browser.find_element(By.XPATH,"//div[@id = 'centerCol']//ul[contains(@class,'a-unordered-list')]")
         first_section_options = [(list_item.get_attribute('id'),list_item.get_attribute('class'),list_item.get_attribute('title').split('Click to select ')[1]) for list_item in first_section.find_elements(By.XPATH,".//li")]
-        print("First Section",first_section_options)
+        # print("First Section",first_section_options)
         for first_section_item in first_section_options:
 
             browser.find_element(By.XPATH,f"//li[@id ='{first_section_item[0]}']").click()
             second_section = browser.find_elements(By.XPATH,"//div[@id = 'centerCol']//ul[contains(@class,'a-unordered-list')]")[1]
             second_section_options = [(list_item.get_attribute('id'),list_item.get_attribute('class'),list_item.get_attribute('title').split('Click to select ')[1]) for list_item in second_section.find_elements(By.XPATH,".//li")]
-            print('Second Section',second_section_options)
+            # print('Second Section',second_section_options)
             for second_section_option in second_section_options:
                 if second_section_option[1] != 'swatchUnavailable':
 
                     browser.find_element(By.XPATH,f"//li[@id = '{second_section_option[0]}']").click()
                     third_section = browser.find_elements(By.XPATH,"//div[@id = 'centerCol']//ul[contains(@class,'a-unordered-list')]")[2]
                     third_section_options = [(list_item.get_attribute('id'),list_item.get_attribute('class'),list_item.get_attribute('title').split('Click to select ')[1]) for list_item in third_section.find_elements(By.XPATH,".//li")]
-                    print('Third Section',third_section_options)
+                    # print('Third Section',third_section_options)
                     for third_section_option in third_section_options:
                         if third_section_option[1]  != 'swatchUnavailable':
 
@@ -192,6 +194,7 @@ for item in ASIN_LIST:
                             attribute_dimensions = [first_section_item[0].split('_')[0],second_section_option[0].split('_')[0],third_section_option[0].split('_')[0]]
                             print('Attributes detected:',first_section_item[0].split('_')[0],',',second_section_option[0].split('_')[0],',',third_section_option[0].split('_')[0])
                             scrape_elements(attribute_dimensions)
+                            browser.refresh()
     else:
         scrape_elements([])
         
@@ -206,6 +209,6 @@ for data_row in data:
 
 
 df=pd.DataFrame(data,columns=['ASIN','Title','Description','Display Price','Fastest Delivery Date','Shipping Charge','Stock','Color','Size','Style',
-                              'Image_1','Image_2','Image_3','Image_4','Image_5','Image_6','Image_6','Image_7','Image_8','Image_9'])
+                              'Image_1','Image_2','Image_3','Image_4','Image_5','Image_6','Image_7','Image_8','Image_9'])
 df.to_csv('amazon-test.csv',index=False)
 print('CSV created under sellvia-catalog-products.csv')
