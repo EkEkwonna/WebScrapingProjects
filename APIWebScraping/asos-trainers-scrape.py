0fa8cbddd8b000fd3cc348c939cf464854aaf0ae
EkEkwonna <kennyekwonna@gmail.com>
2024-08-31 16:39:23 +0100

    Scraping from API backend adding instructions

---
+++ APIWebScraping/asos-trainers-scrape.py
@@ -0,0 +1,16 @@
+"Testing using the backend API to extract product details"
+
+"""
+- Inspect Website 
+- Network 
+- Filter for 'XHR' or 'Fetch/XHR'
+- find the row that has a 'Response' containing product information 
+(usually appears in a JSON structure, you can try clicking 'LOAD MORE' 
+at buttom of the page and usually appears as a new row)
+
+Once located 
+copy the Name with 'Copy as Curl'
+
+Open Postman: 
+- New Environment 
+"""
