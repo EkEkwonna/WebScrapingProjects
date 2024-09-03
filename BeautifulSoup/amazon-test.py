e8d385c21eabd68c5e6363c3f18167ba884d42bd
EkEkwonna <kennyekwonna@gmail.com>
2024-09-03 22:01:48 +0100

    updating class

--- BeautifulSoup/amazon-test.py
+++ BeautifulSoup/amazon-test.py
@@ -1,16 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 import pandas as pd
 import random 
 import time
+import json 
 
 
 class Amazon_product:
@@ -20,45 +13,44 @@ class Amazon_product:
     print(response.status_code)
     self.ASIN = ASIN_VALUE
     self.Title =  soup.select_one('#productTitle').text.strip()
-    self.Description = soup.find('ul',class_='a-unordered-list a-vertical a-spacing-mini').text
+    self.Description = soup.find('ul',class_='a-unordered-list a-vertical a-spacing-mini').text.strip()
     self.DisplayPrice = ''
     self.FastedDeliveryDate = ''
     
     if soup.find('div',class_='a-section a-spacing-none a-padding-none') != None :
-       if soup.soup.find('div',class_='a-section a-spacing-none a-padding-none').find('span',class_='a-price-whole') != None:
-            self.DisplayPrice = soup.find('div',class_='a-section a-spacing-none a-padding-none').find('span',class_='a-price-whole').text + '.' + soup.find('div',class_='a-section a-spacing-none a-padding-none').find('span',class_='a-price-fraction')
+       if soup.find('div',class_='a-section a-spacing-none a-padding-none').find('span',class_='a-price-whole') != None:
+            self.DisplayPrice = soup.find('div',class_='a-section a-spacing-none a-padding-none').find('span',class_='a-price-whole').text.strip() + '.' + soup.find('div',class_='a-section a-spacing-none a-padding-none').find('span',class_='a-price-fraction')
        
     
     if soup.find('div',class_='a-section a-spacing-none a-padding-none') !=None:
-       self.ShippingCharge = soup.find('div',class_='a-section a-spacing-none a-padding-none').find('span',class_='a-size-base a-color-secondary').text
+       self.ShippingCharge = soup.find('div',class_='a-section a-spacing-none a-padding-none').find('span',class_='a-size-base a-color-secondary').text.strip()
     else:
-       self.ShippingCharge = soup.find('div',class_='a-section a-spacing-none a-padding-none').find('span',class_ = 'a-color-error').text
+       self.ShippingCharge = soup.find('span',class_='a-color-price').text.strip()
     
-    self.Stock = soup.select_one('#availability')
+    self.Stock = soup.select_one('#availability').text.strip()
     [self.Color,self.Size ,self.Style] = ['','','']
 
     for attribute in ['size','color','style']:
-       if soup(f'#variation_{attribute}_name') != None:
+       if soup.select_one(f'#variation_{attribute}_name') != None:
             if attribute == 'size':
-                self.Size = soup.select_one(f'#variation_{attribute}_name').find('span',class_='selection').text
+                self.Size = soup.select_one(f'#variation_{attribute}_name').find('span',class_='selection').text.strip()
             if attribute == 'color':
-                self.Color = soup.select_one(f'#variation_{attribute}_name').find('span',class_='selection').text
+                self.Color = soup.select_one(f'#variation_{attribute}_name').find('span',class_='selection').text.strip()
             if attribute == 'style':
-                self.Style = soup.select_one(f'#variation_{attribute}_name').find('span',class_='selection').text
+                self.Style = soup.select_one(f'#variation_{attribute}_name').find('span',class_='selection').text.strip()
 
-    self.Images = collect_images()
-    self.row = [self.ASIN,self.Title,self.Description,self.DisplayPrice,self.FastedDeliveryDate,
-                self.ShippingCharge,self.Stock,self.Color, self.Size ,self.Style ,self.Images]
+    "Extracting Images"
+    img_div = soup.find(id="imgTagWrapperId")
+    imgs_str = img_div.img.get('data-a-dynamic-image')  
+    imgs_dict = json.loads(imgs_str)
+    self.Images = list(imgs_dict.keys())
     
-    print([self.ASIN,self.Title,self.Description,self.DisplayPrice,self.FastedDeliveryDate,
-           self.ShippingCharge ,self.Stock,self.Color, self.Size ,self.Style ,self.Images])
+  def row(self):
+     print([self.ASIN,self.Title,self.Description,self.DisplayPrice,self.FastedDeliveryDate,self.ShippingCharge,self.Stock,self.Color, self.Size ,self.Style ,self.Images])
+     return [self.ASIN,self.Title,self.Description,self.DisplayPrice,self.FastedDeliveryDate,self.ShippingCharge,self.Stock,self.Color, self.Size ,self.Style ,self.Images]
+    
 
-  def collect_images():
-    output_array.append(soup.select_one('#landingImage').Attributes.get('src'))
-
-    output_array = []
-    return output_array
-
+    
 custom_headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
                   'Accept-Language':'en-GB,en-US;q=0.9,en;q=0.8'}
 
@@ -68,7 +60,10 @@ ASIN_LIST = [
     ]
 
 for ASIN_VALUE in ASIN_LIST: 
+    "THE HTML UPDATES the list items with different ASIN codes for all available options"
+    "All you have to do is find all combinations and then find elements once they are displayed and select the ones that are displayed"
     item = Amazon_product(ASIN_VALUE)
+    item.row()
 
 
 
